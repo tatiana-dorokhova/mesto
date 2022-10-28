cb9516844aad8268591f6ec48b81bf321b853537
Tatiana Dorokhova <tatjana.dorokhova@yandex.ru>
2022-10-28 15:50:38 +0300

    не работает добавление новой карточки, возможно не вешается листнер

---
+++ src/components/Section.js
@@ -0,0 +1,23 @@
+export default class Section {
+  // items - начальный массив данных, полученные на вход при создании экземпляра класса
+  // containerSelector - контейнер, в который добавляются отрисованные items
+  constructor({
+    items,
+    renderer
+  }, containerSelector) {
+    this._initialItems = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  // публичный метод, который рендерит весь массив items
+  renderItems() {
+    this._initialItems.forEach(item => this._renderer(item));
+  }
+
+  // публичный метод, который добавляет один отрендеренный элемент в контейнер
+  addItem(element) {
+    console.log('element = ', element);
+    this._container.prepend(element);
+  }
+}
